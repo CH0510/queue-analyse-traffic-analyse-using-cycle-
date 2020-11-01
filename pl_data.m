2eff9d124e28181aabc562fd07104215679e7bca
CH0510 <44310435+CH0510@users.noreply.github.com>
2020-11-01 13:16:20 +0800

    Add files via upload

---
+++ pl_data.m
@@ -0,0 +1,29 @@
+clear;clc;
+H = 2;    %平均服务时间
+C=5;   %信道数
+N=2000;  %用户数
+
+lamda_data = load('C:\Users\hp\Desktop\轨道课设C++循环版\mu_2\lamda_data.txt');
+ration_data = load('C:\Users\hp\Desktop\轨道课设C++循环版\mu_2\block_rate.txt');
+lamda_data = [0;lamda_data];
+ration_data = [0;ration_data];
+plot(lamda_data*H,ration_data,'r','linewidth',1);
+
+
+lamda_temp=0.01:0.01:N*0.01;
+A=lamda_temp*H;
+for j=1:length(A)
+    sum=0.0;
+    for i=1:1:C
+        temp=(A(j)^i)/factorial(i);
+        sum= sum+temp;
+    end
+    Pr(j)=(A(j)^C)/(factorial(C)*sum); %呼损率公式
+end
+hold on;
+plot(A, Pr, 'b')
+title(['呼叫过程拥塞概率的仿真结果,平均服务时间：',num2str(H)]);
+xlabel('话务量( Elang)');
+ylabel('拥塞概率');
+legend('仿真曲线','理论曲线');
+grid on;
